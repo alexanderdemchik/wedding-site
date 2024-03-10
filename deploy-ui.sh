17c78cf293787ac4e1c2c4ffe8298ea371009a7f
Aliaksandr Dzemchyk <alexanderdemchik672@gmail.com>
2024-03-10 23:38:02 +0300

    configure deploy ui

--- deploy-ui.sh
+++ deploy-ui.sh
@@ -3,3 +3,9 @@ git pull origin master
 
 # build ui
 docker build -o "./dist" -f Dockerfile.ui .
+
+rm /data/www/alexandkhris.com/*
+
+cp -a ./dist /data/www/alexandkhris.com/
+
+service nginx reload

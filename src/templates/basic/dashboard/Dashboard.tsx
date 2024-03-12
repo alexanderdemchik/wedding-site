b41735d6ab2c1c9b8edb19a136e876c204de9454
Aliaksandr Dzemchyk <alexanderdemchik672@gmail.com>
2024-03-13 00:48:04 +0300

    Fix lint

--- src/templates/basic/dashboard/Dashboard.tsx
+++ src/templates/basic/dashboard/Dashboard.tsx
@@ -25,7 +25,6 @@ export const Dashboard = () => {
     },
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const forms = useQuery({
     queryFn: () => {
       return FormsService.formsControllerFindAll();

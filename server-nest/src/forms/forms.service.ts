940e9739b5fa8b55a3f6becadb58a7853043dcdd
Aliaksandr Dzemchyk <alexanderdemchik672@gmail.com>
2024-04-10 01:42:39 +0300

    up

--- server-nest/src/forms/forms.service.ts
+++ server-nest/src/forms/forms.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { FormDto } from './dto/form.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Form } from './entities/form.entity';
@@ -17,7 +17,13 @@ export class FormsService {
   }
 
   async findOne(id: string) {
-    return new FormDto(await this.formModel.findOne({ _id: id }).exec());
+    const data = await this.formModel.findOne({ _id: id }).exec();
+
+    if (!data) {
+      throw NotFoundException;
+    }
+
+    return new FormDto(data);
   }
 
   async update(id: string, updateFormDto: FormDto) {
